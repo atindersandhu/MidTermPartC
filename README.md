ceaf3025169a023725515eb540f0c482eb77285b
atindersandhu <sin11288@sheridancollege.ca>
2022-02-23 10:27:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MidTermPartC
+ 
---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,11 @@
+package midtermexam_versiona_extensioncode;
+
+public class TestUserprofile {
+	public static void main(String[] args) {
+		
+		UserProfile profile = new UserProfile("Atinder", "Comedy");
+		
+		System.out.println("Your userProfile has been created");
+	}
+
+}
